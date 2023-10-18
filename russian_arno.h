67568a3ab063f2bee7a13b8f2152c73da6d97fb7
ay4 <dima.neiaglov@gmail.com>
2023-10-18 20:49:20 +0400

    First commit

---
+++ russian_arno.h
@@ -0,0 +1,36 @@
+
+#pragma once
+#include QMK_KEYBOARD_H
+
+#define RU_A KC_F      // А
+#define RU_BE KC_COMM  // Б
+#define RU_V KC_D      // В
+#define RU_G KC_U      // Г
+#define RU_D KC_L      // Д
+#define RU_ZH KC_SCLN  // Ж
+#define RU_Z KC_P      // З
+#define RU_E KC_T      // Е
+#define RU_I KC_B      // И
+#define RU_J KC_Q      // Й
+#define RU_K KC_R      // К
+#define RU_L KC_K      // Л
+#define RU_M KC_V      // М
+#define RU_N KC_Y      // Н
+#define RU_O KC_J      // О
+#define RU_PE KC_G     // П
+#define RU_R KC_H      // Р
+#define RU_S KC_C      // С
+#define RU_T KC_N      // Т
+#define RU_U KC_E      // У
+#define RU_F KC_A      // Ф
+#define RU_KH KC_LBRC  // Х
+#define RU_TS KC_W     // Ц
+#define RU_CH KC_X     // Ч
+#define RU_SH KC_I     // Ш
+#define RU_SCH KC_O    // Щ
+#define RU_HARD KC_RBRC // Ъ
+#define RU_Y KC_S      // Ы
+#define RU_SOFT KC_M   // Ь
+#define RU_UE KC_QUOTE // Э
+#define RU_YU KC_DOT   // Ю
+#define RU_YA KC_Z     // Я
---
+++ symbols.h
@@ -0,0 +1,32 @@
+#pragma once
+#include QMK_KEYBOARD_H
+
+#define AY_DLR S(KC_4)     // $
+#define AY_UNDR S(KC_MINS) // _
+#define AY_LBRC KC_LBRC    // [
+#define AY_MINS KC_MINS    // -
+#define AY_MDASH A(S(KC_MINS)) // ---
+#define AY_AT S(KC_2)      // @
+#define AY_PLUS S(KC_EQL)  // +
+#define AY_LPRN S(KC_9)    // (
+#define AY_COMM KC_COMM    // ,
+#define AY_SCLN KC_SCLN    // ;
+#define AY_EQL KC_EQL      // =
+#define AY_LCORN S(KC_COMM) // <
+#define AY_LCURL S(KC_LBRC) // {
+#define AY_APOS KC_QUOT     // '
+#define AY_QUOTE S(KC_QUOT) // "
+#define AY_SLSH KC_SLSH     // /
+#define AY_EXCL S(KC_1)     // !
+#define AY_RBRC KC_RBRC     // ]
+#define AY_STAR S(KC_8)     // * 
+#define AY_PERC S(KC_5)     // %
+#define AY_CLN S(KC_SCLN)   // :
+#define AY_DOT KC_DOT       // .
+#define AY_RPRN S(KC_0)    // )
+#define AY_HASH S(KC_3)     // #
+#define AY_AMPER S(KC_7)    // &
+#define AY_TILDE S(KC_GRV)  // ~
+#define AY_QUEST S(KC_SLSH) // ?
+#define AY_RCURL S(KC_RBRC) // }
+#define AY_RCORN S(KC_DOT) // <
